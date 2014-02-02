87eb54789aa411cf9e498e8d252bc0a7e0d97cd2
James Brooks <jbrooksuk@me.com>
2014-02-02 20:40:11 +0000

    Working on Pebble commands

--- Pebble.py
+++ Pebble.py
@@ -1,5 +1,23 @@
 import sublime, sublime_plugin
+import subprocess
+import os
 
-class SublimePebble(sublime_plugin.EventListener)
-	def __init__(self):
-		
+class pebblebuild(sublime_plugin.WindowCommand):
+	def run(self):
+		cmd = ["pebble", "build"]
+		self.window.run_command("exec", { "cmd": cmd })
+
+class pebbleclean(sublime_plugin.WindowCommand):
+	def run(self):
+		cmd = ["pebble", "clean"]
+		self.window.run_command("exec", { "cmd": cmd })
+
+class pebbleinstall(sublime_plugin.WindowCommand):
+	def run(self):
+		cmd = ["pebble", "install"]
+		self.window.run_command("exec", { "cmd": cmd })
+
+class pebblescreenshot(sublime_plugin.WindowCommand):
+	def run(self):
+		cmd = ["pebble", "screenshot"]
+		self.window.run_command("exec", { "cmd": cmd })
